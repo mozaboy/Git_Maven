e562ddd34cfe25c496578fac372699c69981026c
wuqing <mozaboy@163.com>
2016-06-15 10:25:27 +0800

    新增学习笔记

---
+++ wuqing-book-web/src/main/java/com/wuqing/book/service/HelloService.java
@@ -0,0 +1,9 @@
+package com.wuqing.book.service;
+
+/**
+ * Created by Administrator on 2016/6/14.
+ */
+public interface HelloService {
+
+    public void say(String str);
+}
---
+++ wuqing-book-web/src/main/java/com/wuqing/book/service/impl/HelloServiceImpl.java
@@ -0,0 +1,14 @@
+package com.wuqing.book.service.impl;
+
+import com.wuqing.book.service.HelloService;
+
+/**
+ * Created by Administrator on 2016/6/14.
+ */
+public class HelloServiceImpl implements HelloService {
+
+    @Override
+    public void say(String str) {
+        System.out.print("aaa");
+    }
+}
